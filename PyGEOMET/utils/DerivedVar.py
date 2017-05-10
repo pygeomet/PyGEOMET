97aa6540579aac039708394a34c373046e26b7c0
freitagb <brian.freitag@nsstc.uah.edu>
2017-05-10 15:29:06 -0500

    added theta to derived variable list

--- PyGEOMET/utils/DerivedVar.py
+++ PyGEOMET/utils/DerivedVar.py
@@ -631,6 +631,7 @@ class WRFDerivedVar:
 
     def ptheta(self):
         self.var = self.theta
+        self.var2 = self.theta
         self.varTitle = r"$\theta$ (K)$" + self.dataSet.getTime()
 
     def theta_e(self):
